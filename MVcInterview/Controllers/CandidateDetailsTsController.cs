8a55d914d482b35caf5c139936b9afa463aefd95
jiten <jitendra.kumar@handleexception.in>
2022-09-20 19:52:51 +0530

    Committed

--- MVcInterview/Controllers/CandidateDetailsTsController.cs
+++ MVcInterview/Controllers/CandidateDetailsTsController.cs
@@ -6,15 +6,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MVcInterview.Models.CandidateDetailsT;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Microsoft.AspNetCore.Hosting;
+using static System.Net.WebRequestMethods;
 
 namespace MVcInterview.Controllers
 {
     public class CandidateDetailsTsController : Controller
     {
+        private IConfiguration Configuration;
         private readonly JeetuContext _context;
-
-        public CandidateDetailsTsController(JeetuContext context)
+        
+        public CandidateDetailsTsController(JeetuContext context,IConfiguration _configuration)
         {
+            Configuration = _configuration;
             _context = context;
         }
 
@@ -53,15 +61,99 @@ namespace MVcInterview.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CandidateId,CandidateName,CandidateExperience,CandidatePhoneNo,CandidateMailId,CandidateSkills,CreatedDate")] CandidateDetailsT candidateDetailsT)
+        public async Task<IActionResult> Create([Bind("CandidateId,CandidateName,CandidateExperience,CandidatePhoneNo,CandidateMailId,CandidateSkills,CreatedDate")] CandidateDetailsT candidateDetailsT, List<IFormFile> files)
         {
             if (ModelState.IsValid)
             {
                 _context.Add(candidateDetailsT);
                 await _context.SaveChangesAsync();
+                
+
+                if (files != null)
+                {
+                    //long size = files.Sum(f => f.Length);
+                    string path = "";
+                    var filePaths = new List<string>();
+                    foreach (var formFile in files)
+                    {
+                        if (formFile.Length > 0)
+                        {
+                            //string filename = Path.GetExtension(formFile.FileName);
+                            string filename =DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss").Replace(":","-").Replace(" ","_")+"_" + formFile.FileName;
+                            path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "ResumeFilesUploaded"));
+                            using (var filestream = new FileStream(Path.Combine(path, filename), FileMode.Create))
+                            {
+                                await formFile.CopyToAsync(filestream);
+
+
+                                string ConnStr = this.Configuration.GetConnectionString("DefaultConnection");
+                                SqlConnection _con = new SqlConnection(ConnStr);
+                                SqlCommand cmd = new SqlCommand("Usp_CandidateFiles", _con);
+                                cmd.CommandType = CommandType.StoredProcedure;
+                                cmd.Parameters.AddWithValue("@candidateid", candidateDetailsT.CandidateId);
+                                cmd.Parameters.AddWithValue("@CandidatefileName", filename);
+                                cmd.Parameters.AddWithValue("@Candidatefilepath", path);
+                                cmd.Parameters.AddWithValue("@Tran", "Save");
+                                _con.Open();
+                                cmd.ExecuteNonQuery();
+                                _con.Close();
+                            }
+                        }
+                    }
+                }
+
                 return RedirectToAction(nameof(Index));
             }
+            // string id = TempData["CandidateId"].ToString();
             return View(candidateDetailsT);
+            //return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CandidateUploadfile(List<IFormFile> files)
+        {
+            if (files != null)
+            {
+                //long size = files.Sum(f => f.Length);
+                string path = "";
+                var filePaths = new List<string>();
+                foreach (var formFile in files)
+                {
+                    if (formFile.Length > 0)
+                    {
+                        string filename = Guid.NewGuid() + Path.GetExtension(formFile.FileName);
+                        path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "ResumeFilesUploaded"));
+                        using (var filestream = new FileStream(Path.Combine(path, filename), FileMode.Create))
+                        {
+                            await formFile.CopyToAsync(filestream);
+
+                        }
+                    }
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+            [HttpPost]
+        public JsonResult Save(CandidateDetailsT _candetails)
+        {
+            string ConnStr = this.Configuration.GetConnectionString("DefaultConnection");
+            SqlConnection _con = new SqlConnection(ConnStr);
+            SqlCommand cmd = new SqlCommand("Usp_Candidate", _con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@candidateid", int.Parse("0"));
+            cmd.Parameters.AddWithValue("@CandidateName", _candetails.CandidateName);
+            cmd.Parameters.AddWithValue("@CandidateExp", _candetails.CandidateExperience);
+            cmd.Parameters.AddWithValue("@CandidatePhoneNo", _candetails.CandidatePhoneNo);
+            cmd.Parameters.AddWithValue("@CandidateMailid", _candetails.CandidateMailId);
+            cmd.Parameters.AddWithValue("@CandidateSkills", _candetails.CandidateSkills);
+            cmd.Parameters.AddWithValue("@Tran", "Save");
+            cmd.Parameters.Add("@CanSaveid", SqlDbType.Int);
+            cmd.Parameters["@CanSaveid"].Direction = ParameterDirection.Output;
+            _con.Open();
+            cmd.ExecuteNonQuery();
+            _con.Close();
+            string CandidateId =cmd.Parameters["@CanSaveid"].Value.ToString();
+            //TempData["CandidateId"] = CandidateId;
+            return Json(CandidateId, new Newtonsoft.Json.JsonSerializerSettings());
         }
 
         // GET: CandidateDetailsTs/Edit/5
--- MVcInterview/Models/CandidateDetailsT/CandidateDetailsT.cs
+++ MVcInterview/Models/CandidateDetailsT/CandidateDetailsT.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVcInterview.Models.CandidateDetailsT
 {
     public partial class CandidateDetailsT
     {
+
         public int CandidateId { get; set; }
         public string CandidateName { get; set; } = null!;
         public string CandidateExperience { get; set; } = null!;
         public string CandidatePhoneNo { get; set; } = null!;
         public string CandidateMailId { get; set; } = null!;
         public string CandidateSkills { get; set; } = null!;
-        public DateTime CreatedDate { get; set; }
+
+        [Display(Name = "CreatedDate")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0: MM/dd/YYYY}")]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
     }
 }
